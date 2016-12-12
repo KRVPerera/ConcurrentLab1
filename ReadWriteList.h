296f1a2aba88632b308e97a46b5f8f1e984a2ba0
damith <damith.w.12@cse.mrt.ac.lk>
2016-12-12 07:42:02 +0530

    Adding ReadWriteList.h header file

---
+++ ReadWriteList.h
@@ -0,0 +1,33 @@
+//
+// Created by damith on 12/12/16.
+//
+
+#ifndef CONCURRENTTHLAB_1_READWRITELIST_H
+#define CONCURRENTTHLAB_1_READWRITELIST_H
+
+#include "SerialDriver.h"
+#include <pthread.h>
+
+class ReadWriteList : public SerialList{
+
+private:
+    pthread_rwlock_t glb_rwlock;
+
+protected:
+    Node *Find(int i) override;
+
+public:
+    ReadWriteList();
+
+    void Print();
+
+    void Delete(int i) override;
+
+    bool Member(int i) override;
+
+    void Insert(int i) override;
+
+    ~ReadWriteList() ;
+};
+
+#endif //CONCURRENTTHLAB_1_READWRITELIST_H
