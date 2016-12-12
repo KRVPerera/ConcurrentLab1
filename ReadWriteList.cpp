8e6e557027971f7710baf7ee9d25a1574e8564ef
damith <damith.w.12@cse.mrt.ac.lk>
2016-12-12 07:42:36 +0530

    Adding ReadWriteList.cpp file

---
+++ ReadWriteList.cpp
@@ -0,0 +1,45 @@
+//
+// Created by damith on 12/12/16.
+//
+
+#include "ReadWriteList.h"
+
+ReadWriteList::ReadWriteList() : SerialList() {
+    pthread_rwlock_init(&glb_rwlock, NULL);
+}
+
+void ReadWriteList::Print() {
+    pthread_rwlock_rdlock(&glb_rwlock);
+    SerialList::Print();
+    pthread_rwlock_unlock(&glb_rwlock);
+}
+
+ReadWriteList::~ReadWriteList() {
+    pthread_rwlock_destroy(&glb_rwlock);
+}
+
+Node *ReadWriteList::Find(int i) {
+    pthread_rwlock_rdlock(&glb_rwlock);
+    Node *found = SerialList::Find(i);
+    pthread_rwlock_unlock(&glb_rwlock);
+    return found;
+}
+
+bool ReadWriteList::Member(int i) {
+    pthread_rwlock_rdlock(&glb_rwlock);
+    bool found = SerialList::Member(i);
+    pthread_rwlock_unlock(&glb_rwlock);
+    return found;
+}
+
+void ReadWriteList::Insert(int i) {
+    pthread_rwlock_wrlock(&glb_rwlock);
+    SerialList::Insert(i);
+    pthread_rwlock_unlock(&glb_rwlock);
+}
+
+void ReadWriteList::Delete(int i) {
+    pthread_rwlock_wrlock(&glb_rwlock);
+    SerialList::Delete(i);
+    pthread_rwlock_unlock(&glb_rwlock);
+}
