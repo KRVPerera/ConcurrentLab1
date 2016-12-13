27702355615f484cf798857b31415f8cbbf5c866
KRVPerera <rukshan.viduranga@gmail.com>
2016-12-13 15:27:23 +0530

    Duplicated values accepted
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

--- SerialDriver.cpp
+++ SerialDriver.cpp
@@ -17,11 +17,7 @@ void SerialDriver::Drive() {
 
     struct timespec t0, t1;
     unsigned long sec, nsec;
-
-
     int small_sample_size = 10;
-    float comp_time;
-
     float mean;
     float sd;
     int req_n;
@@ -42,57 +38,27 @@ void SerialDriver::Drive() {
             vector<int> generatedValues; // used to generate unique values
             srand(time(NULL));
             vector<float> times(small_sample_size);
-            populate_list(&list, &generatedValues, num_population);
-
+            populate_list(&list, &generatedValues, num_population);GET_TIME(t0);
             for (int j = 0; j < num_operations; j++) { // 10000 operation
                 int randi = rand() % 3;
                 int rand_num = rand() % 65534 + 1;
-                bool present = true;
+
                 switch (randi) {
                     case 0:
                         if (num_insert_f > 0) {
-                            while (present) {
-                                present = false;
-                                rand_num = rand() % 65534 + 1;
-                                for (int k = 0; k < generatedValues.size(); k++) {
-                                    if (generatedValues[k] == rand_num) {
-                                        present = true;
-                                        break;
-                                    }
-                                }
-                            }
-
-                            GET_TIME(t0);
-                            list.Insert(rand_num);GET_TIME(t1);
-                            generatedValues.push_back(rand_num);
-                            comp_time = Util::elapsed_time_msec(&t0, &t1, &sec, &nsec);
-                            times.push_back(comp_time);
+                            list.Insert(rand_num);
                             num_insert_f--;
                         }
                         break;
                     case 1:
-                        if (num_delete_f > 0) { GET_TIME(t0);
-                            list.Delete(rand_num);GET_TIME(t1);
-                            int delid = -1;
-                            for (int l = 0; l <= generatedValues.size(); l++) {
-                                if (generatedValues[l] == rand_num) {
-                                    delid = l;
-                                    break;
-                                }
-                            }
-                            if (delid != -1) {
-                                generatedValues.erase(generatedValues.begin() + delid);
-                            }
-                            comp_time = Util::elapsed_time_msec(&t0, &t1, &sec, &nsec);
-                            times.push_back(comp_time);
+                        if (num_delete_f > 0) {
+                            list.Delete(rand_num);
                             num_delete_f--;
                         }
                         break;
                     case 2:
-                        if (num_member_f > 0) { GET_TIME(t0);
-                            list.Member(rand_num);GET_TIME(t1);
-                            comp_time = Util::elapsed_time_msec(&t0, &t1, &sec, &nsec);
-                            times.push_back(comp_time);
+                        if (num_member_f > 0) {
+                            list.Member(rand_num);
                             num_member_f--;
                         }
                         break;
@@ -100,11 +66,8 @@ void SerialDriver::Drive() {
                         cerr << "Invalid random function call" << endl;
                         break;
                 }
-            }
-            float sum_time = 0;
-            for (int m = 0; m < times.size(); ++m) {
-                sum_time += times[m];
-            }
+            }GET_TIME(t1);
+            float sum_time = Util::elapsed_time_msec(&t0, &t1, &sec, &nsec);
             tot_times.push_back(sum_time);
 
         }
